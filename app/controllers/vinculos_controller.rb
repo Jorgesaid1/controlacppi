b6fcf496a76943f71f8b3a33173f303b08d965ee
Jorge Gonzalez <jorgesaid1@hotmail.com>
2024-05-14 09:34:52 -0600

    Creacion de vinculos con nueva actividad

--- app/controllers/vinculos_controller.rb
+++ app/controllers/vinculos_controller.rb
@@ -1,4 +1,26 @@
 class VinculosController < ApplicationController
   def new
+    @empleado = Empleado.find(params[:empleado_id])
+    @homeoffice = Homeoffice.find(params[:homeoffice_id])
+    @registroho = Registroho.find(params[:registroho_id])
+    @vinculo = Vinculo.new
+  end
+
+  def create
+    @empleado = Empleado.find(params[:empleado_id])
+    @homeoffice = Homeoffice.find(params[:homeoffice_id])
+    @registroho = Registroho.find(params[:registroho_id])
+    @vinculo = Vinculo.new(vinculo_params)
+    @vinculo.registroho = @registroho
+    if @vinculo.save
+      redirect_to empleado_path(@empleado)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def vinculo_params
+    params.require(:vinculo).permit(:comentarios, :num_horas, :actividad_id)
   end
 end
